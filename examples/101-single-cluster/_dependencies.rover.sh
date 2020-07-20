a1af7263c6947577816e3d9dfe158d453294ecd9
Hieu Nguyen Nhu <hieunhu@microsoft.com>
2020-07-21 01:26:47 +0800

    Update TENANT_ID/TENANT_NAME for all Readmes

--- examples/101-single-cluster/_dependencies.rover.sh
+++ examples/101-single-cluster/_dependencies.rover.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rover --clone-folder /landingzones/launchpad --clone-branch vnext13
 rover --clone-folder /landingzones/landingzone_caf_foundations --clone-branch vnext13
 rover --clone-folder /landingzones/landingzone_networking --clone-branch vnext13
