24d8eb888f7ce54c228d960bf0b5dc881ddb18fa
Arnaud Lheureux <arnaud@microsoft.com>
2021-01-18 03:02:22 +0000

    Module version and azurerm upgrade

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.40"
+      version = "~> 2.43"
     }
     azuread = {
       source  = "hashicorp/azuread"
